6bcea5b150b685f6e401b20cd3e7df9de40e0f8f
Federico Perazzi <fperazzi@adobe.com>
2018-07-09 20:42:03 -0700

    Get data using absolute path

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+DATADIR="$( cd ../"$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 wget https://www.dropbox.com/s/2nsklcbm4hkiuvf/proSR-data.zip
-unzip proSR-data.zip
+unzip proSR-data.zip $DATADIR
 rm -rf proSR-data.zip
+
